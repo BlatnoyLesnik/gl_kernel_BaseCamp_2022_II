be078d67bd12da70c81a047d083cf39e019766d3
Romka <dinaroma5323@gmail.com>
2023-01-30 13:52:15 +0200

    Task_3_1: Change: makefile(GCC or CLANG), main.c(fix mistakes of input)
    Iplemented possibility build program not only GCC but also clang.
    Fixed input because program take int number into short variable.
    
    Signed-off-by: Romka <dinaroma5323@gmail.com>

--- task3-make/main.c
+++ task3-make/main.c
@@ -7,7 +7,7 @@ int main()
   uint16_t UserNum = 0xFFFF, QuessNum = 0xFFFF;
 
   printf("Please enter number from 0 to 9: ");
-  scanf("%d", &UserNum);
+  scanf("%hd", &UserNum);
   if (UserNum <= 9) {
     QuessNum = randomize();
     if (UserNum == QuessNum) {
